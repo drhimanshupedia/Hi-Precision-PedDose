da9c6bfaa61ed57ff60c85eefea47d7fca4b3403
drhimanshupedia <himanshupatil211@gmail.com>
2026-02-19 16:05:15 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,23 @@
-const CACHE_NAME = "peddose-cache-v1";
-const urlsToCache = [
-  "index.html",
-  "manifest.json",
-  "icon-192.png",
-  "icon-512.png"
+const CACHE_NAME = 'peddose-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
 ];
 
 // Install Service Worker
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
     })
   );
 });
 
-// Fetch from cache first, then network
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Update Service Worker
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      );
+// Fetch Assets from Cache
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
     })
   );
 });
